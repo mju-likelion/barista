567d3a023eb242524dafb3947ddce023db7432a4
ndaemy <yuyaebean@gmail.com>
2021-12-26 23:40:05 +0900

    💄 Make sign in page beautiful

--- app/routes/sign-in.tsx
+++ app/routes/sign-in.tsx
@@ -1,4 +1,7 @@
+import { Button, Center } from "@chakra-ui/react";
 import { useEffect } from "react";
+import { FaGoogle } from "react-icons/fa";
+import Particles from "react-tsparticles";
 import { fb } from "~/firebase-config";
 
 export default function SignIn() {
@@ -12,7 +15,67 @@ export default function SignIn() {
 
   return (
     <>
-      <button onClick={fb.signIn}>Sign In with Google</button>
+      <Center h="100vh">
+        <Button
+          colorScheme="gray"
+          leftIcon={<FaGoogle />}
+          variant="solid"
+          onClick={fb.signIn}
+        >
+          구글로 로그인하기
+        </Button>
+      </Center>
+      <Particles
+        options={{
+          background: {
+            color: {
+              value: "transparent",
+            },
+          },
+          fpsLimit: 60,
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 50,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
     </>
   );
 }
